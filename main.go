67f6416c59febf1f5d9147f6f7a4f0b183df63b1
Viktor Mörsell <viktor@upper.st>
2021-07-01 22:27:18 +0200

    Add CLI boilerplate

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	cli "github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name: "Jira CLI",
+		Commands: []*cli.Command{
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
